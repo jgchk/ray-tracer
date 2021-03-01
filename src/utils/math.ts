6b567809fe4add3a912cc96b32ee08b840ca142c
Jake Cheek <jake@f-m.fm>
2021-03-01 14:58:38 -0500

    🎉 hello world!

--- src/utils/math.ts
+++
@@ -1,7 +0,0 @@
-export const clamp =
-  (min: number) => (max: number) => (x: number) =>
-  x < min ? min : (x > max ? max : x)
-
-export const degreesToRadians =
-  (degrees: number) =>
-  degrees * Math.PI / 180
--- src/utils/random.ts
+++
@@ -1,6 +0,0 @@
-export const random =
-  Math.random
-
-export const randomRange =
-  (min: number) => (max: number) =>
-  min + (max-min)*random()
